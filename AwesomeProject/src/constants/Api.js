08d1394d67c3953ad2a0f3dc519fd12c1df6c681
chao <1207461954@qq.com>
2020-11-06 15:50:35 +0800

    12

--- AwesomeProject/src/constants/Api.js
+++
@@ -1,4 +0,0 @@
-const dev = false;
-const Api = dev ? 'http://10.0.0.2:3000' : 'https://clwy.cn';
-
-export default Api;
--- AwesomeProject/src/screens/user/SplashScreen.js
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import Loading from '../../components/shared/Loading';
-
-function SplashScreen({route, navigation}) {
-  return <Loading />;
-}
-
-export default SplashScreen;
